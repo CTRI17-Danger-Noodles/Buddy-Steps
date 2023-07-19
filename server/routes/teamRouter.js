ddb234f07cc055ce5f371fae5c2bfc6ba7ae8a31
Rylie <ryles@MacBook-Pro-4.ht.home>
2023-07-18 20:29:44 -0700

    updated user route and controller to include password authentication

--- server/routes/teamRouter.js
+++ server/routes/teamRouter.js
@@ -1,8 +1,29 @@
 const express = require('express');
-// const userController = require('../controller/userController');
+const teamController = require('../controller/teamController');
 
 const router = express.Router();
 
+// get information about all tasks associated with a team
+router.post('/', teamController.getTasks, (req, res) => {
+  res.status(200).json(res.locals.teamTasks);
+  /*
+    response: 
+    {
+        tasks: [
+            {
+                name:
+                genre:
+                status:
+                startDate:
+                endDate: 
+                users: [array of users]
+            },
+            {},
+            {}
+        ]
+    } 
 
+  */ 
+}); 
 
 module.exports = router;
